58a52ca11201b05839fb25af0ffce2cda52c00a5
JosefKarim <josefkarim@live.se>
2022-11-03 02:58:12 +0100

    första upplagan

---
+++ Infrastructure/ToDoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoListMVC.Models;
+
+namespace ToDoListMVC.Infrastructure
+{
+    public class ToDoContext: DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TodoList> ToDoList { get; set; }
+    }
+}
